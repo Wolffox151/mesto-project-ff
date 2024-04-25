e1cf5198a1657d04c752cc208883e4b4c414be4c
Wolffox151 <72281946+Wolffox151@users.noreply.github.com>
2024-04-25 17:12:11 +0300

    Project 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const templateCard = document.querySelector('#card-template').content.querySelector('.places__item');
 
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+const createCard = (data) => {
+  const newCard = templateCard.cloneNode(true);
+  const imageCard = newCard.querySelector('.card__image').setAttribute('src', data.link);
+  const titleCard = newCard.querySelector('.card__title');
+  const deleteButton = newCard.querySelector('.card__delete-button')
 
+  titleCard.textContent = data.name;
+
+  deleteButton.addEventListener('click', () => {
+    newCard.remove();
+  });
+
+  return newCard;
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((data) => {
+  const newCard = createCard(data);
+  placesList.append(newCard)
+})
