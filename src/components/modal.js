2413b200d604f170c79b969027abb6c5753e235d
Wolffox151 <72281946+Wolffox151@users.noreply.github.com>
2024-07-02 10:32:47 +0300

    Update

--- src/components/modal.js
+++ src/components/modal.js
@@ -12,10 +12,11 @@ export const closeModal = (popup) => {
   document.removeEventListener('keyup', closePopupOnEscape);
 }
 
-export const closePopupOnOverlayClick= (evt) => {
-  if (!evt.target.closest('.popup__content') || evt.target.classList.contains('popup__close')) {
-    closeModal(document.querySelector('.popup_is-opened'))
-  }
+export const closePopupOnOverlayClick = (evt) => { 
+  console.log(evt.target.classList)
+  if (evt.target.classList.contains('popup_is-opened') || evt.target.classList.contains('popup__close')) { 
+    closeModal(document.querySelector('.popup_is-opened')); 
+  } 
 }
 
 export const closePopupOnEscape =(evt) => {
